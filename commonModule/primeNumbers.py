756be1d3ecb76232ed00aaa247b41ec34d23bf60
Kamil Wodowski <kamilwodowski@o2.pl>
2026-01-12 12:54:05 +0100

    finish generating prime numbers

--- commonModule/primeNumbers.py
+++ commonModule/primeNumbers.py
@@ -1,5 +1,6 @@
 import math
 import random
+import secrets
 
 import utils.cryptomath
 
@@ -48,18 +49,66 @@ def isPrimeViaFermatTest(n):
     power = pow(b, n-1, n)
     return power == 1
 
-def isPrimeViaRabinMillerTest(n):
-    pass
+def isPrimeViaRabinMillerTest(n, trials = 5):
+    t = n - 1
+    s = 0
+    while t % 2 == 0:
+        t /= 2
+        s += 1
+    for trial in range(0, trials):
+        b = random.randrange(2, n)
+        power = pow(b, int(t), n)
 
-def isPrimeNumberViaRabinMillerTest(n):
-    pass
+        if power != 1:
+            vec = []
+            for i in range(0, s):
+                power_ = pow(b, int(t * 2 ** i), n)
+                vec.append(power_)
+            if n - 1 not in vec:
+                return False
+    return True
+
+def isPrimeNumber(n, omitPrimesNumber = 100, rabinMillerTrails = 10):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else:
+        LOW_PRIMES = findPrimeNumbersViaEratostenesSieve(omitPrimesNumber)
+
+        if n > omitPrimesNumber:
+            for prime in LOW_PRIMES:
+                if n % prime == 0:
+                    return False
+
+        return isPrimeViaRabinMillerTest(n, rabinMillerTrails)
+
+def generateLargePrime(keySize, omitPrimesNumber = 100, rabinMillerTrails = 10):
+    isPrime = False
+    triesCount = 0
+    while not isPrime:
+        triesCount += 1
+
+        primeCandidate = secrets.randbits(keySize)
+
+        if primeCandidate % 2 == 0:
+            continue
+
+        primeCandidate |= (1 << (keySize - 1))
+
+        isPrime = isPrimeNumber(primeCandidate, omitPrimesNumber, rabinMillerTrails)
+
+    print('Prime trial count: ', triesCount)
+    return primeCandidate
 
 if __name__ == '__main__':
-    # res = isPrimeNumberViaDivisionTest(1000007)
+    # res = isPrimeNumber(104729)
     # print(res)
 
-    # res = findPrimeNumbersViaEratostenesSieve(18)
-    # print(res)
+    # it takes while to generate prime for 128
+    primeNumber = generateLargePrime(64)
 
-    res = isPrimeViaFermatTest(561)
-    print(res)
+    print('Prime found: ', primeNumber)
+    print('Is prime fr:', isPrimeNumber(primeNumber))
